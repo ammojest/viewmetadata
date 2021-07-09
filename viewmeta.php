483be0c79d57443696bceba63db82ce45dd6fa38
Ammojest <63594225+ammojest@users.noreply.github.com>
2021-07-09 10:10:24 +0100

    Create viewmeta.php

---
+++ viewmeta.php
@@ -0,0 +1,53 @@
+/**
+ * Display all post meta
+ *
+ */
+add_action( 'wp_footer', function() {
+	if( is_single() )
+		ea_pp( get_post_meta( get_the_ID() ) );
+});
+
+/**
+ * Pretty Printing
+ *
+ */
+function ea_pp( $obj, $label = '' ) {
+	$data = json_encode( print_r( $obj,true ) );
+	?>
+	<style type="text/css">
+		#bsdLogger {
+		position: absolute;
+		top: 30px;
+		right: 0px;
+		border-left: 4px solid #bbb;
+		padding: 6px;
+		background: white;
+		color: #444;
+		z-index: 999;
+		font-size: 1.25em;
+		width: 400px;
+		height: 800px;
+		overflow: scroll;
+		}
+	</style>
+	<script type="text/javascript">
+		var doStuff = function(){
+			var obj = <?php echo $data; ?>;
+			var logger = document.getElementById('bsdLogger');
+			if (!logger) {
+				logger = document.createElement('div');
+				logger.id = 'bsdLogger';
+				document.body.appendChild(logger);
+			}
+			////console.log(obj);
+			var pre = document.createElement('pre');
+			var h2 = document.createElement('h2');
+			pre.innerHTML = obj;
+			h2.innerHTML = '<?php echo addslashes($label); ?>';
+			logger.appendChild(h2);
+			logger.appendChild(pre);
+		};
+		window.addEventListener ("DOMContentLoaded", doStuff, false);
+	</script>
+	<?php
+}
